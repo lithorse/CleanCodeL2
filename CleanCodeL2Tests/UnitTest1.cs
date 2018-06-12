2a48ff801e908c3c8fcfa315d707d58aa6a53a70
rikard <rikard.hohlfalt@gmail.com>
2018-06-12 13:18:10 +0200

    Add project files.

---
+++ CleanCodeL2Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using CleanCodeL2;
+
+namespace CleanCodeL2Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
